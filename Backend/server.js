4af8b78cf032b9318161dc9f6acfa52e6191e40d
Rss <rssvinaykumar3801@gmail.com>
2026-02-03 09:18:15 +0530

    Updated CORS urls

--- Backend/server.js
+++ Backend/server.js
@@ -16,11 +16,19 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://trello-seven-pi.vercel.app"
+];
 
 app.use(cors({
-  origin: [
-    "http://localhost:5173",
-  ],
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   credentials: true
 }));
 
